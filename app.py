ceb3e867c271f0d8162631f91f060c8a773060dd
Kevin Gonzalez <krg@tutamail.com>
2020-09-20 14:22:12 -0700

    single user movie logging app

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
+import datetime
 import database
+from typing import List, Tuple
+
+# Movie = (title, release_timestamp, watched)
+Movie = Tuple[str, int, int]
 
 menu = """Please select one of the following options:
 1) Add new movie
@@ -21,26 +26,57 @@ welcome = "Welcome to the watchlist app!"
 print(welcome)
 database.create_tables()
 
+
+def prompt_add_movie() -> (str, int):
+    title = input("Movie title: ")
+    release_date = input("Release date (YYYY-mm-dd): ")
+    parsed_date = datetime.datetime.strptime(release_date, "%Y-%m-%d")
+    # NOTE: Windows won't parse anything before the Unix epoch.
+    timestamp = parsed_date.timestamp()
+
+    return (title, timestamp)
+
+
+def print_movie_list(heading: str, movies: List[Movie]):
+    print(f"--- {heading} MOVIES ---")
+    for movie in movies:
+        movie_date = datetime.datetime.fromtimestamp(movie[1])
+        human_date = movie_date.strftime("%Y %b %d")
+        print(f"*) {movie[0]} (released: {human_date})")
+    print("-----------------------\n")
+
+
+def prompt_watch_movie() -> str:
+    title = input("Enter title of movie watched: ")
+
+    return title
+
+
+def print_watched(title: str):
+    (title, _, watched) = database.get_movie(title)
+    print(f"You've now watched \"{title}\" {watched} times.")
+
 while (user_input := input(menu)) != EXIT:
     if user_input == ADD_MOVIE:
-        title = input("Movie title: ")
-        release_date = input("Release date (YYYY-mm-dd): ")
+        (title, release_date) = prompt_add_movie()
         database.add_movie(title, release_date)
 
     elif user_input == VIEW_UPCOMING:
-        print(database.get_movies(upcoming=True))
+        movies = database.get_movies(upcoming=True)
+        print_movie_list("UPCOMING", movies)
 
     elif user_input == VIEW_ALL:
-        print(database.get_movies(upcoming=False))
+        movies = database.get_movies(upcoming=False)
+        print_movie_list("ALL", movies)
 
     elif user_input == WATCH_MOVIE:
-        title = input("Movie title: ")
+        title = prompt_watch_movie()
         database.watch_movie(title)
-        (title, _, watched) = database.get_movie(title)
-        print(f"You've now watched \"{title}\" {watched} times.")
+        print_watched(title)
 
     elif user_input == VIEW_WATCHED:
-        print(database.get_watched_movies())
+        movies = database.get_watched_movies()
+        print_movie_list("WATCHED", movies)
 
     else:
         print("Invalid input, please try again!")
