ef9ac865c79383196a8d80ecd3b6302109f1555b
Kevin Gonzalez <krg@tutamail.com>
2020-09-20 11:30:59 -0700

    connect to a db and implement basic functionality

--- app.py
+++ app.py
@@ -9,22 +9,38 @@ menu = """Please select one of the following options:
 6) Exit
 
 Your selection: """
+ADD_MOVIE = '1'
+VIEW_UPCOMING = '2'
+VIEW_ALL = '3'
+WATCH_MOVIE = '4'
+VIEW_WATCHED = '5'
+EXIT = '6'
 welcome = "Welcome to the watchlist app!"
 
 
 print(welcome)
 database.create_tables()
 
-while (user_input := input(menu)) != "6":
-    if user_input == "1":
-        pass
-    elif user_input == "2":
-        pass
-    elif user_input == "3":
-        pass
-    elif user_input == "4":
-        pass
-    elif user_input == "5":
-        pass
+while (user_input := input(menu)) != EXIT:
+    if user_input == ADD_MOVIE:
+        title = input("Movie title: ")
+        release_date = input("Release date (YYYY-mm-dd): ")
+        database.add_movie(title, release_date)
+
+    elif user_input == VIEW_UPCOMING:
+        print(database.get_movies(upcoming=True))
+
+    elif user_input == VIEW_ALL:
+        print(database.get_movies(upcoming=False))
+
+    elif user_input == WATCH_MOVIE:
+        title = input("Movie title: ")
+        database.watch_movie(title)
+        (title, _, watched) = database.get_movie(title)
+        print(f"You've now watched \"{title}\" {watched} times.")
+
+    elif user_input == VIEW_WATCHED:
+        print(database.get_watched_movies())
+
     else:
         print("Invalid input, please try again!")
--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 # title: string, release_date: timestamp, watched: integer
+import datetime
 import sqlite3
 
 CREATE_MOVIES_TABLE = """CREATE TABLE IF NOT EXISTS movies(
@@ -11,9 +12,46 @@ INSERT_MOVIES = """INSERT INTO movies (
     release_timestamp, 
     watched
 ) VALUES (?, ?, 0);"""
+WATCH_MOVIE = "UPDATE movies SET watched = ? WHERE title = ?;"
+SELECT_MOVIE = "SELECT * FROM movies WHERE title = ?;"
 SELECT_ALL_MOVIES = "SELECT * FROM movies;"
 SELECT_UPCOMING_MOVIES = "SELECT * FROM movies WHERE release_timestamp > ?;"
 SELECT_WATCHED_MOVIES = "SELECT * FROM movies WHERE watched >= 1;"
 
+connection = sqlite3.connect("data.db")
+
 def create_tables():
-    pass
+    with connection:
+        connection.execute(CREATE_MOVIES_TABLE)
+
+# TODO: validate 'release_timestamp' so that it's valid POSIX time.
+def add_movie(title: str, release_timestamp: int):
+    with connection:
+        connection.execute(INSERT_MOVIES, (title, release_timestamp))
+
+def watch_movie(title: str):
+    cursor = connection.execute(SELECT_MOVIE, (title,)).fetchone()
+    current_watched = cursor[2]
+    with connection:
+        connection.execute(WATCH_MOVIE, (current_watched + 1, title))
+
+def get_movie(title: str):
+    cursor = connection.execute(SELECT_MOVIE, (title,)).fetchone()
+
+    return cursor
+
+def get_movies(upcoming: bool = False):
+    cursor = connection.cursor()
+
+    if not upcoming:
+        cursor.execute(SELECT_ALL_MOVIES)
+    else:
+        today_timestamp = datetime.datetime.today().timestamp() 
+        cursor.execute(SELECT_UPCOMING_MOVIES, (today_timestamp,))
+    
+    return cursor.fetchall()
+
+def get_watched_movies():
+    cursor = connection.execute(SELECT_WATCHED_MOVIES)
+
+    return cursor.fetchall()
